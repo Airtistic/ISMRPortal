d8290b4d7d6756826d5bac00a3503c8cf744275a
Rafael Fernandes <rafael.fernandes@28736-mbpr.home>
2015-12-26 21:19:57 -0500

    Adding get API example

--- start.js
+++ start.js
@@ -5,10 +5,21 @@ var app = express();
 var www = express.static(__dirname);
 app.use('/', www);
 
+app.get('/api/articles', getArticlesEn);
+
+function getArticlesEn(req, res){
+  res.send([
+    {title:'News 1', contentUs:'This is awesome! 1'},
+    {title:'News 2', contentUs:'This is awesome! 2'},
+    {title:'News 3', contentUs:'This is awesome! 3'},
+    {title:'News 4', contentUs:'This is awesome! 4'},
+    {title:'News 4', contentUs:'This is awesome! 5'}
+  ])
+}
+
 app.listen(8888);
 console.log('successfully started!!');
 console.log('----------------------');
 console.log('http://localhost:8888/');
 console.log('----------------------');
 console.log('ctrl+c to stop');
-
